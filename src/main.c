fb0191ff841378ab89bd18e3ae788b9d64a05125
skifli <121291719+skifli@users.noreply.github.com>
2023-03-21 17:35:09 +0000

    feat: cleanup for rewrite in c

---
+++ src/main.c
@@ -0,0 +1,8 @@
+// Part of the Exeme Language Project, under the MIT license. See '/LICENSE' for
+// license information. SPDX-License-Identifier: MIT License.
+
+#include "stdio.h"
+
+#pragma pack(1)
+
+int main(void) { printf("hello world\n"); }
