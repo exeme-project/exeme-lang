7dc7323269e86e6cf0f449155c8c8c9276976bde
skifli <121291719+skifli@users.noreply.github.com>
2023-06-10 18:32:19 +0100

    feat: add AST template for parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -9,3 +9,84 @@
 
 #include "../lexer/lexer.c"
 #include "../utils/errors.c"
+
+/**
+ * Represents an AST.
+ */
+struct AST {
+	enum {
+		AST_VARIABLE,
+	} IDENTIFIER;
+	union {
+		struct AST_VARIABLE {
+			const struct LexerToken *_TOKEN;
+			const struct String *NAME;
+		} AST_VARIABLE;
+	} data;
+};
+
+#define AST_STRUCT_SIZE sizeof(struct AST)
+
+struct AST *ast_new(struct AST ast) {
+	struct AST *self = malloc(AST_STRUCT_SIZE);
+
+	if (!self) {
+		panic("failed to malloc AST struct");
+	}
+
+	*self = ast; // Copy to heap
+	return self;
+}
+
+#define AST_NEW(type, ...)                                                     \
+	ast_new((struct AST){type, {.type = (struct type){__VA_ARGS__}}})
+
+/**
+ * Represents a parser.
+ */
+struct Parser {
+	struct Lexer *lexer;
+};
+
+#define PARSER_STRUCT_SIZE sizeof(struct Parser)
+
+/**
+ * Creates a new Parser struct.
+ *
+ * @param FILE_PATH The path of the file to parse.
+ *
+ * @return The created Parser struct.
+ */
+struct Parser *parser_new(const char *FILE_PATH) {
+	struct Parser *self = malloc(PARSER_STRUCT_SIZE);
+
+	if (!self) {
+		panic("failed to malloc Parser struct");
+	}
+
+	self->lexer = lexer_new(FILE_PATH);
+
+	return self;
+}
+
+/**
+ * Frees a Parser struct.
+ *
+ * @param self The current Parser struct.
+ */
+void parser_free(struct Parser *self) {
+	if (self) {
+		lexer_free(self->lexer);
+
+		free(self);
+		self = NULL;
+	} else {
+		panic("Parser struct has already been freed");
+	}
+}
+
+bool parser_parse() {
+	struct AST *term = AST_NEW(AST_VARIABLE, NULL, NULL);
+
+	return false;
+}
